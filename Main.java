b68c35dfc3f9be6dc1a63230b1e80dfb0dc57da9
Soumean <tongsoumean246@gmail.com>
2026-06-16 16:00:40 +0700

    lolololol

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
-public class Main extends Application{
-    //
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mini_project04.fxml"));
+        primaryStage.setTitle("Project 4");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
